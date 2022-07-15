1d35c10e99577001d358dd5c34073bf6add07ee1
Khalil KACIM <kkacim@palo-it.com>
2022-07-15 15:59:31 +0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    arrowParens: 'avoid',
+    jsxSingleQuote: true,
+}
---
+++ src/Module1EntryPoint.tsx
@@ -0,0 +1,5 @@
+function Module1EntryPoint() {
+  return <div>I'm module 1</div>
+}
+
+export default Module1EntryPoint
